2ae2044cba2e8ec738a5d21c586c3150d2a8aab4
JFjerm <jacob.fjermestad@gmail.com>
2014-01-07 15:42:14 -0700

    First Commit
    First Commit with the first problem.
    
    Hoping to define a structure for each solution, as well as a way to test
    whether or not the solution is correct.

---
+++ ProjectEuler/EulerMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler.Solutions;
+
+namespace ProjectEuler
+{
+    class EulerMain
+    {
+        static void Main(string[] args)
+        {
+            Euler1();
+        }
+
+        private static void Euler1()
+        {
+            var problem1 = new Problem1();
+            problem1.SolveProblem();
+            Console.WriteLine(problem1.Solution);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectEuler/Solutions/Problem1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Solutions
+{
+    //Solution class to Multiples of 3 and 5
+    //Link: http://projecteuler.net/problem=1
+    public class Problem1
+    {
+        public int Solution { get; private set; }
+        private const int MAX_INTEGER = 999;
+
+        public void SolveProblem()
+        {
+            Solution = AddMultiples(FindMultiples(MAX_INTEGER));
+        }
+        private List<int> FindMultiples(int number)
+        {
+            var multiples = new List<int>();
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    multiples.Add(i);
+                }
+            }
+
+            return multiples;
+        }
+        private int AddMultiples(List<int> multiples)
+        {
+            var sum = 0;
+
+            foreach (int multiple in multiples)
+            {
+                sum += multiple;
+            }
+
+            return sum;
+        }
+    }
+}
